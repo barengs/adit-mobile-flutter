6510a9c90202bd803c6a0066dc27f9ab625bdf71
Makdum <mkdmvx9@gmail.com>
2025-05-01 08:52:44 +0700

    Refactor: Pindahkan widget CustomBottomNavBar ke file widget terpisah

--- lib/screen-2/dashboard.dart
+++ lib/screen-2/dashboard.dart
@@ -1,4 +1,6 @@
+// lib/dashboard.dart
 import 'package:flutter/material.dart';
+import 'package:siakad/widgets/custom_bottom_nav_bar.dart'; // Import custom widget
 
 class Dashboard extends StatefulWidget {
   const Dashboard({super.key});
@@ -8,6 +10,7 @@ class Dashboard extends StatefulWidget {
 
 class _DashboardState extends State<Dashboard> {
   int _currentIndex = 0;
+
   final List<Widget> _pages = [
     Center(child: Text('Dashboard')),
     Center(child: Text('QR Page')),
@@ -19,83 +22,14 @@ class _DashboardState extends State<Dashboard> {
     return Scaffold(
       appBar: AppBar(title: Text('Dashboard')),
       body: _pages[_currentIndex],
-      bottomNavigationBar: SafeArea(
-        child: Container(
-          height: 58, // Maintained at 58 as required
-          decoration: BoxDecoration(
-            boxShadow: [
-              BoxShadow(
-                color: const Color.fromARGB(66, 110, 110, 110),
-                blurRadius: 2,
-                offset: Offset(0, -1),
-              ),
-            ],
-          ),
-          child: BottomNavigationBar(
-            currentIndex: _currentIndex,
-            onTap: (int index) {
-              setState(() {
-                _currentIndex = index;
-              });
-            },
-            selectedItemColor: Color(0xFF21ABA5),
-            unselectedItemColor: Colors.grey,
-            showSelectedLabels: false,
-            showUnselectedLabels: false,
-            elevation: 0,
-            backgroundColor: Colors.white,
-            type: BottomNavigationBarType.fixed,
-            selectedFontSize: 0,
-            unselectedFontSize: 0,
-            items: [
-              _buildBottomNavigationBarItem(Icons.home_filled, 0),
-              _buildBottomNavigationBarItem(Icons.filter_center_focus_sharp, 1),
-              _buildBottomNavigationBarItem(Icons.message, 2),
-            ],
-          ),
-        ),
+      bottomNavigationBar: CustomBottomNavBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
       ),
-    );
-  }
-
-  BottomNavigationBarItem _buildBottomNavigationBarItem(
-    IconData icon,
-    int index,
-  ) {
-    bool isActive = _currentIndex == index;
-    return BottomNavigationBarItem(
-      icon: Container(
-        height: 58,
-        padding: EdgeInsets.zero,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            icon == Icons.message
-                ? Transform(
-                  alignment: Alignment.center,
-                  transform: Matrix4.rotationY(3.1416),
-                  child: Icon(
-                    icon,
-                    color: isActive ? Color(0xFF21ABA5) : Colors.grey,
-                    size: 29,
-                  ),
-                )
-                : Icon(
-                  icon,
-                  color: isActive ? Color(0xFF21ABA5) : Colors.grey,
-                  size: 29,
-                ),
-            SizedBox(height: 1), // Reduced spacing
-            Container(
-              height: 3.0,
-              width: 25.0,
-              color: isActive ? Color(0xFF21ABA5) : Colors.transparent,
-            ),
-          ],
-        ),
-      ),
-      label: '',
     );
   }
 }
---
+++ lib/widgets/custom_bottom_nav_bar.dart
@@ -0,0 +1,86 @@
+// lib/widgets/custom_bottom_nav_bar.dart
+import 'package:flutter/material.dart';
+
+class CustomBottomNavBar extends StatelessWidget {
+  final int currentIndex;
+  final Function(int) onTap;
+
+  const CustomBottomNavBar({
+    Key? key,
+    required this.currentIndex,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Container(
+        height: 58,
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: const Color.fromARGB(66, 110, 110, 110),
+              blurRadius: 2,
+              offset: Offset(0, -1),
+            ),
+          ],
+        ),
+        child: BottomNavigationBar(
+          currentIndex: currentIndex,
+          onTap: onTap,
+          selectedItemColor: Color(0xFF21ABA5),
+          unselectedItemColor: Colors.grey,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          elevation: 0,
+          backgroundColor: Colors.white,
+          type: BottomNavigationBarType.fixed,
+          selectedFontSize: 0,
+          unselectedFontSize: 0,
+          items: [
+            _buildItem(Icons.home_filled, 0),
+            _buildItem(Icons.filter_center_focus_sharp, 1),
+            _buildItem(Icons.message, 2),
+          ],
+        ),
+      ),
+    );
+  }
+
+  BottomNavigationBarItem _buildItem(IconData icon, int index) {
+    bool isActive = currentIndex == index;
+
+    return BottomNavigationBarItem(
+      icon: Container(
+        height: 58,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            icon == Icons.message
+                ? Transform(
+                  alignment: Alignment.center,
+                  transform: Matrix4.rotationY(3.1416),
+                  child: Icon(
+                    icon,
+                    color: isActive ? Color(0xFF21ABA5) : Colors.grey,
+                    size: 29,
+                  ),
+                )
+                : Icon(
+                  icon,
+                  color: isActive ? Color(0xFF21ABA5) : Colors.grey,
+                  size: 29,
+                ),
+            SizedBox(height: 1),
+            Container(
+              height: 3.0,
+              width: 25.0,
+              color: isActive ? Color(0xFF21ABA5) : Colors.transparent,
+            ),
+          ],
+        ),
+      ),
+      label: '',
+    );
+  }
+}
