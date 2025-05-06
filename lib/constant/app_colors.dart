b572e23b8f1b5fb83f90ce19e66f975225d6c7c0
Makdum <mkdmvx9@gmail.com>
2025-05-06 21:21:23 +0700

    FEAT :  buat sistem warna untuk warna aplikasi

---
+++ lib/constant/app_colors.dart
@@ -0,0 +1,10 @@
+// lib/constants/app_colors.dart
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF21ABA5);
+
+  static const Color textWhite = Color(0xFFFFFFFF);
+
+  static const Color accent = Color(0xFF2DBAB1);
+}
