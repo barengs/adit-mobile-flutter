8d2435c8e8b23f1c22af66f0300d8427d24f07a1
Makdum <mkdmvx9@gmail.com>
2025-05-07 20:26:13 +0700

    test halaman

--- lib/dashboard/dashboard_content.dart
+++ lib/dashboard/dashboard_content.dart
@@ -3,18 +3,31 @@ import 'package:flutter/material.dart';
 class DashboardContent extends StatelessWidget {
   const DashboardContent({super.key});
 
+  final String imagePath = 'assets/images/istockphoto-1393542225-170667a.jpg'; // Path relatif ke asset
+
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        ClipRRect(
-          borderRadius: BorderRadius.circular(12),
-          child: Image.network(
-            'https://images.unsplash.com/photo-1507525428034-b723cf961d3e',
-            height: 150,
-            width: double.infinity,
-            fit: BoxFit.cover,
+        Container(
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: Colors.blue, // Warna border
+              width: 2.0,       // Ketebalan border
+            ),
+            borderRadius: BorderRadius.circular(12), // Radius yang sama dengan ClipRRect agar border ikut melengkung
+          ),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(12),
+            child: Image.asset(
+              imagePath,
+              height: 150,
+              width: double.infinity,
+              fit: BoxFit.cover,
+              errorBuilder: (context, error, stackTrace) =>
+                  const Text('Gambar tidak ditemukan!'),
+            ),
           ),
         ),
         const SizedBox(height: 12),
