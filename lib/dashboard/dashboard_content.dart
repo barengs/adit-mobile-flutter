07f12194867596ce307cfe38aa2f042a005001ad
Makdum <mkdmvx9@gmail.com>
2025-05-10 21:21:44 +0700

    UPDATE : tambahkan alamat kampus

--- lib/dashboard/dashboard_content.dart
+++ lib/dashboard/dashboard_content.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:siakad/constant/app_colors.dart';
+import 'package:siakad/widgets/aktivitas_card.dart';
 
 class DashboardContent extends StatelessWidget {
   const DashboardContent({super.key});
 
-  final String imagePath = 'assets/images/istockphoto-1393542225-170667a.jpg'; // Path relatif ke asset
+  final String imagePath = 'assets/images/maxresdefault.jpg';
 
   @override
   Widget build(BuildContext context) {
@@ -11,31 +13,49 @@ class DashboardContent extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Container(
-          decoration: BoxDecoration(
-            border: Border.all(
-              color: Colors.blue, // Warna border
-              width: 2.0,       // Ketebalan border
-            ),
-            borderRadius: BorderRadius.circular(12), // Radius yang sama dengan ClipRRect agar border ikut melengkung
-          ),
+          decoration: BoxDecoration(borderRadius: BorderRadius.circular(12)),
           child: ClipRRect(
-            borderRadius: BorderRadius.circular(12),
+            borderRadius: BorderRadius.circular(25),
             child: Image.asset(
               imagePath,
               height: 150,
               width: double.infinity,
               fit: BoxFit.cover,
-              errorBuilder: (context, error, stackTrace) =>
-                  const Text('Gambar tidak ditemukan!'),
+              errorBuilder:
+                  (context, error, stackTrace) =>
+                      const Text('Gambar tidak ditemukan!'),
             ),
           ),
         ),
         const SizedBox(height: 12),
         const Text(
           'Harvard Kampus 1',
-          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          style: TextStyle(
+            fontSize: 16,
+            fontFamily: 'Sarabun',
+            fontWeight: FontWeight.w700,
+          ),
         ),
-        const SizedBox(height: 8),
+        const SizedBox(height: 5),
+        const Text(
+          'Jl. Siliwangi (Ringroad Utara) Jombor Sleman D.I Yogyakarta',
+          style: TextStyle(
+            fontSize: 10.0,
+            fontFamily: 'Roboto',
+            fontWeight: FontWeight.w300,
+          ),
+        ),
+        const SizedBox(height: 10),
+        Container(
+          width: 54,
+          height: 5,
+          decoration: BoxDecoration(
+            color: AppColors.primary,
+            borderRadius: BorderRadius.circular(4),
+          ),
+        ),
+        const SizedBox(height: 10),
+        AktivitasCard()
       ],
     );
   }
