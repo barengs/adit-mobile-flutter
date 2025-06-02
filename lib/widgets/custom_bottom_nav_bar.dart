2b90d869091074a435b05b694f8c0ba1404bcbe4
Makdum <mkdmvx9@gmail.com>
2025-06-02 21:12:52 +0700

    FIX : perbaiki lebar bottom navigation agar penuh di mode landscape

--- lib/widgets/custom_bottom_nav_bar.dart
+++ lib/widgets/custom_bottom_nav_bar.dart
@@ -13,74 +13,71 @@ class CustomBottomNavBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Container(
-        height: 58,
-        decoration: BoxDecoration(
-          boxShadow: [
-            BoxShadow(
-              color: const Color.fromARGB(66, 110, 110, 110),
-              blurRadius: 2,
-              offset: Offset(0, -1),
-            ),
-          ],
-        ),
-        child: BottomNavigationBar(
-          currentIndex: currentIndex,
-          onTap: onTap,
-          selectedItemColor: Color(0xFF21ABA5),
-          unselectedItemColor: Colors.grey,
-          showSelectedLabels: false,
-          showUnselectedLabels: false,
-          elevation: 0,
-          backgroundColor: Colors.white,
-          type: BottomNavigationBarType.fixed,
-          selectedFontSize: 0,
-          unselectedFontSize: 0,
-          items: [
-            _buildItem(Icons.home_filled, 0),
-            _buildItem(Icons.filter_center_focus_sharp, 1),
-            _buildItem(Icons.message, 2),
-          ],
+    return Container(
+      width: double.infinity, // Memastikan lebar penuh
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: const Color.fromARGB(66, 110, 110, 110),
+            blurRadius: 2,
+            offset: Offset(0, -1),
+          ),
+        ],
+      ),
+      child: SafeArea(
+        child: SizedBox(
+          height: 58,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              _buildNavItem(Icons.home_filled, 0),
+              _buildNavItem(Icons.filter_center_focus_sharp, 1),
+              _buildNavItem(Icons.message, 2),
+            ],
+          ),
         ),
       ),
     );
   }
 
-  BottomNavigationBarItem _buildItem(IconData icon, int index) {
+  Widget _buildNavItem(IconData icon, int index) {
     bool isActive = currentIndex == index;
-
-    return BottomNavigationBarItem(
-      icon: SizedBox(
-        height: 58,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            icon == Icons.message
-                ? Transform(
-                  alignment: Alignment.center,
-                  transform: Matrix4.rotationY(3.1416),
-                  child: Icon(
-                    icon,
-                    color: isActive ? Color(0xFF21ABA5) : Colors.grey,
-                    size: 29,
-                  ),
-                )
-                : Icon(
-                  icon,
-                  color: isActive ? Color(0xFF21ABA5) : Colors.grey,
-                  size: 29,
-                ),
-            SizedBox(height: 1),
-            Container(
-              height: 3.0,
-              width: 25.0,
-              color: isActive ? Color(0xFF21ABA5) : Colors.transparent,
-            ),
-          ],
+    return Expanded(
+      child: InkWell(
+        onTap: () => onTap(index),
+        child: SizedBox(
+          height: 58,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              icon == Icons.message
+                  ? Transform(
+                      alignment: Alignment.center,
+                      transform: Matrix4.rotationY(3.1416),
+                      child: Icon(
+                        icon,
+                        color: isActive ? Color(0xFF21ABA5) : Colors.grey,
+                        size: 29,
+                      ),
+                    )
+                  : Icon(
+                      icon,
+                      color: isActive ? Color(0xFF21ABA5) : Colors.grey,
+                      size: 29,
+                    ),
+              SizedBox(height: 1),
+              Container(
+                height: 3.0,
+                width: 25.0,
+                color: isActive ? Color(0xFF21ABA5) : Colors.transparent,
+              ),
+            ],
+          ),
         ),
       ),
-      label: '',
     );
   }
+
+
 }
