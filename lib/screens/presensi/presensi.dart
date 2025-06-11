d38f80b33abd05099416c4ae648a870cdcc59e55
Makdum <mkdmvx9@gmail.com>
2025-06-11 16:58:47 +0700

    STYLE : hilangkan padding

--- lib/screens/presensi/presensi.dart
+++ lib/screens/presensi/presensi.dart
@@ -1,18 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:siakad/widgets/presensi_qr_view.dart';
+import 'package:siakad/widgets/switch_button.dart';
+import 'package:siakad/widgets/matkul_presensi_card.dart';
 
-class Presensi extends StatelessWidget {
+class Presensi extends StatefulWidget {
   const Presensi({super.key});
+
+  @override
+  State<Presensi> createState() => _PresensiState();
+}
+
+class _PresensiState extends State<Presensi> {
+  bool isQRMode = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [PresensiQRView()],
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              AnimatedSwitcher(
+                duration: const Duration(milliseconds: 300),
+                transitionBuilder: (child, animation) {
+                  return FadeTransition(opacity: animation, child: child);
+                },
+                child:
+                    isQRMode
+                        ? const PresensiQRView(key: ValueKey('qr'))
+                        : const MatkulPresensiCard(key: ValueKey('manual')),
+              ),
+              const SizedBox(height: 12),
+              SwitchButton(
+                isQRMode: isQRMode,
+                onToggle: (value) {
+                  setState(() {
+                    isQRMode = value;
+                  });
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
