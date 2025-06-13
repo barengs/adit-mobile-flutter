20ddd2e6e326287c55a90626af89625a8509a8c5
Makdum <mkdmvx9@gmail.com>
2025-06-13 15:28:55 +0700

    ADD:  tambahkan card tahun ajaran dan semester

--- lib/screens/khs/khs_page.dart
+++ lib/screens/khs/khs_page.dart
@@ -45,7 +45,94 @@ class _KhsPageState extends State<KhsPage> {
                       SwitchItem(icon: Icons.auto_graph),
                     ],
                   ),
-                  const SizedBox(height: 24),
+                  SizedBox(height: 20),
+
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 12),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        // Kolom Tahun Ajaran
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Text(
+                              'Tahun Ajaran',
+                              style: TextStyle(
+                                fontFamily: 'Roboto',
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500,
+                                color: Color.fromRGBO(0, 0, 0, 0.75),
+                              ),
+                            ),
+                            const SizedBox(height: 4),
+                            Container(
+                              width: 232,
+                              height: 44,
+                              decoration: BoxDecoration(
+                                color: Color.fromRGBO(229, 229, 229, 0.75),
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                              alignment: Alignment.centerRight,
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 12,
+                              ),
+                              child: Text(
+                                '2021/2022',
+                                style: TextStyle(
+                                  fontFamily: 'Roboto',
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w500,
+                                  color: Color.fromRGBO(0, 0, 0, 0.75),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+
+                        const SizedBox(width: 16),
+
+                        // Kolom Semester
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Text(
+                              'Semester',
+                              style: TextStyle(
+                                fontFamily: 'Roboto',
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500,
+                                color: Color.fromRGBO(0, 0, 0, 0.75),
+                              ),
+                            ),
+                            const SizedBox(height: 4),
+                            Container(
+                              width: 126,
+                              height: 44,
+                              decoration: BoxDecoration(
+                                color: Color.fromRGBO(229, 229, 229, 0.75),
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                              alignment: Alignment.centerRight,
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 12,
+                              ),
+                              child: Text(
+                                'Ganjil',
+                                style: TextStyle(
+                                  fontFamily: 'Roboto',
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w500,
+                                  color: Color.fromRGBO(0, 0, 0, 0.75),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
                 ],
               ),
             ),
